e1f3ca56f94ff310ec9fbb794534815f3d43b03d
Hyeonju Hub <dusgho501@gmail.com>
2026-08-30 20:18:41 +0900

    feat: add HYEONJU little universe PWA icons

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,27 @@
   description: "현주님의 YouTube 콘텐츠를 한곳에서 만나보세요.",
   applicationName: "현주님의 작은 우주",
   manifest: "/manifest.webmanifest",
+  icons: {
+    icon: [
+      {
+        url: "/icons/favicon-32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        url: "/icons/icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+    ],
+    apple: [
+      {
+        url: "/icons/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+  },
   appleWebApp: {
     capable: true,
     title: "현주님의 작은 우주",
